cf0c6ce9c4f0dc60151b65804f9ff6b6843858f7
Konstantyn-Tokar <79169268+Konstantyn-Tokar@users.noreply.github.com>
2021-07-26 20:39:49 +0300

    Начал кодить

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+import createMenuMarkup from '../templates/menuCards.hbs';
+import menu from '../data/menu.json';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+  checkbox: document.querySelector('#theme-switch-toggle'),
+  listMenu: document.querySelector('.js-menu'),
+  body: document.body,
+};
+
+refs.checkbox.addEventListener('change', addsStyle);
+
+function addsStyle(e) {
+    if()
+}
+
+console.log(refs.checkbox);
+console.log(refs.listMenu);
+console.log(refs.body);
+console.log(createMenuMarkup(menu));
+
+refs.listMenu.innerHTML = createMenuMarkup(menu);
